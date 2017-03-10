0794744f3d44aed026379a64d737cb74b6af8815
anthony <anthony.bruening@gmail.com>
2017-03-10 16:24:57 +0100

    s10 solved

---
+++ s10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#define LINE_LENGTH 50
+#define LINE_PARSES 2
+
+int main()
+{
+    char str[LINE_PARSES*LINE_LENGTH];
+    int indexPointer = 0 ;
+
+    for(int j=0 ; j<LINE_PARSES ; j++)
+    {
+        for(int i = 0, c ; i < LINE_LENGTH && (c = getchar()) != '\n' ; str[indexPointer++] = c, i++)
+            if(i == (LINE_LENGTH - 1))
+            {
+                printf("Fehler bei der Engabe.\n");
+                return 1 ;
+            }
+    }
+
+    str[indexPointer] = '\0';
+    printf("%s\n", str) ;
+
+    return 0 ;
+}
