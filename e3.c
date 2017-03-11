9f80c4c686161e1ab4d4070cc1a1fdde7a715bdf
anthony <anthony.bruening@gmail.com>
2017-03-11 16:39:42 +0100

    e3 solved

--- e3.c
+++ e3.c
@@ -4,6 +4,6 @@ int main()
 {
     printf("Er kam lässig heran und sagte nur \"Na, wie geht's?\". \n"
                    "Kommentare beginnen mit /* und enden mit */. Verwechseln Sie\n"
-                   "das bitte nicht mit \* bzw. *\!\n");
+                   "das bitte nicht mit \\* bzw. *\\!\n");
     return 0;
 }
